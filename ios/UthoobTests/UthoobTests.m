0737e41b476b1583fe7041636a030c4768158eae
shubhamchepe <hemantchepe@gmail.com>
2023-06-07 19:36:10 +0530

    second commit

--- ios/UthoobTests/UthoobTests.m
+++ ios/UthoobTests/UthoobTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface AwesomeProjectTests : XCTestCase
+@interface UthoobTests : XCTestCase
 
 @end
 
-@implementation AwesomeProjectTests
+@implementation UthoobTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
